67a2ec9d4671c49d9c3605ad39fd85c5a471df33
ldc861117 <ldc.acm@gmail.com>
2025-06-26 20:45:00 +0800

    fix: remove relative import; use global st_echo to avoid extension load error

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,9 @@
-import { st_echo } from '../../../../../script.js'; // fallback to SillyTavern global utils
-
 const extensionName = 'deep-plotter';
 
 function onExtensionLoaded() {
-  if (typeof st_echo === 'function') {
-    st_echo('success', `[${extensionName}] Extension loaded.`);
+  const echo = typeof window !== 'undefined' ? window.st_echo : undefined;
+  if (typeof echo === 'function') {
+    echo('success', `[${extensionName}] Extension loaded.`);
   } else {
     console.log(`[${extensionName}] Extension loaded.`);
   }
